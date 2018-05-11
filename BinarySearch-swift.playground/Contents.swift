02d62ba3bf9114dc053207933ce17d58b6b4f207
William Hu <william_hu@epam.com>
2018-05-11 17:52:13 +0800

    Refactor algorithm.

--- BinarySearch-swift.playground/Contents.swift
+++ BinarySearch-swift.playground/Contents.swift
@@ -6,7 +6,7 @@ extension Array where Element: Comparable {
     func binarySearch(searchedValue: Element, startIndex: Int, endIndex: Int) -> Int{
         
         let mid = (startIndex + endIndex)/2
-        if (mid == 0 || mid == self.count - 1) && self[mid] != searchedValue {
+        if startIndex > endIndex {
             return -1
         }
         if self[mid] == searchedValue {
@@ -17,15 +17,42 @@ extension Array where Element: Comparable {
             return binarySearch(searchedValue: searchedValue, startIndex: startIndex, endIndex: mid-1)
         }
     }
+    
+    func binarySearch2(searchedValue: Element, startIndex: Int, endIndex: Int) -> Int {
+        var start = startIndex
+        var end = endIndex
+        
+        repeat  {
+            let mid = (start + end)/2
+            if self[mid] == searchedValue {
+                return mid
+            }
+            if self[mid] < searchedValue {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        } while (start <= end)
+        return -1
+    }
 }
 
 //Test
 
 let original = [2,3,6,7,8,19]
-let result1 = original.binarySearch(searchedValue: 0, startIndex: 0, endIndex: original.count)
+print("Binary search 1 result:")
+let result1 = original.binarySearch(searchedValue: 0, startIndex: 0, endIndex: original.count - 1)
 print("result:\(result1)")
-let result2 = original.binarySearch(searchedValue:100, startIndex: 0, endIndex: original.count)
+let result2 = original.binarySearch(searchedValue:100, startIndex: 0, endIndex: original.count - 1)
 print("result:\(result2)")
-let result3 = original.binarySearch(searchedValue:8, startIndex: 0, endIndex: original.count)
+let result3 = original.binarySearch(searchedValue:8, startIndex: 0, endIndex: original.count - 1)
 print("result:\(result3)")
+
+print("Binary search 2 result:")
+let result4 = original.binarySearch(searchedValue:0, startIndex: 0, endIndex: original.count - 1)
+print("result:\(result4)")
+let result5 = original.binarySearch(searchedValue:100, startIndex: 0, endIndex: original.count - 1)
+print("result:\(result5)")
+let result6 = original.binarySearch2(searchedValue:8, startIndex: 0, endIndex: original.count - 1)
+print("result:\(result6)")
 
